a32adb5a5f2abe80068a1deebd44c3b842f0e0a0
unknown <jyzhou@umass.edu>
2021-01-11 17:07:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import WholePage from './components/wholePageComponent/wholePage';
-import Home from './components/homeComponent/home';
-import About from './components/aboutComponent/about';
-import {Link} from 'react-scroll';
+
 
 function App() {
   return (
-    <Router>
+    
     <div className="App">
-      <Route path = '/' exact component = {WholePage} />
-      <Route path = '/home' exact component = {Home} />
-      <Route path = '/about' component = {About}/>
+      <WholePage/>
     </div>
-    </Router>
   )
 }
 
